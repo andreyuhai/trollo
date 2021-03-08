112baab2cd83bbded09d8485d861705e7be982b5
andreyuhai <burakaymakci@gmail.com>
2021-03-08 12:11:07 +0100

    Add a route for moving lists around

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,11 @@ end
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
   resources :cards
-  resources :lists
+  resources :lists do
+    member do
+      patch :move
+    end
+  end
 
   root to: 'lists#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
